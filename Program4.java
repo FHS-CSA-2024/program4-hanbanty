0e6577e65b4b105e378515f7f7eda72a94a4b37a
hanbanty <mohakupmanyue@gmail.com>
2024-08-13 09:57:00 -0700

    Update Program4.java
    finished program 4

--- Program4.java
+++ Program4.java
@@ -1,6 +1,47 @@
 //Youre on your own!
 //Jk ask for help if you need it (but also look at previous programs!!)
+import java.util.Scanner;
+ public class Program4{
+     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int num1 = scanner.nextInt();
+        System.out.println("Enter another number:");
+        int num2 = scanner.nextInt();
+        System.out.println("Enter another number:");
+        int num3 = scanner.nextInt();
+        System.out.println("Enter another number:");
+        int num4 = scanner.nextInt();
+        int sum = num1+num2+num3+num4;
+        double average = sum/4.0;
+        System.out.println("\n The sum of the four numbers is " + sum);
+        System.out.println("\n The average of the four numbers is " + average);
+    
+    }
+}
 
+
+//Your code here:
+
+
+
+
+//Paste console output below:
+/*
+Enter a number:
+475
+Enter another number:
+821
+Enter another number:
+369
+Enter another number:
+562
+
+ The sum of the four numbers is 2227
+
+ The average of the four numbers is 556.75
+
+*/
 
 
 //Paste console output below:
